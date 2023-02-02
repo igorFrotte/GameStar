23e3cbae8970e3641f51eaf42002439c05d9e50a
igorFrotte <igorfrotte@gmail.com>
2023-02-01 21:34:46 -0300

    first commit

---
+++ node/index.ts
@@ -0,0 +1,13 @@
+import cors from "cors";
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(cors());
+
+app.get("/status", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(5000, () => {
+  console.log("Running on 5000");
+});

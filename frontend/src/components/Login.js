b81b840a61c55cc9d25c6636ed4833cba2ea4823
igorFrotte <igorfrotte@gmail.com>
2023-02-06 14:37:25 -0300

    login bug fix 1

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,15 +1,20 @@
 import { useNavigate } from "react-router-dom";
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
-import jwt_decode from "jwt-decode";
+//import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+//import jwt_decode from "jwt-decode";
 import { login } from "../services/axiosService";
 
 export default function Login() {
 
   const navigate = useNavigate();
 
-	const sucessGoogle = (response) => {
-		const {name, email, picture , sub } = jwt_decode(response.credential);
-    const userInfo = {name, email, picture, googleId: Number(sub)};
+	const sucessGoogle = () => { // (response)
+		//const {name, email, picture , sub } = jwt_decode(response.credential);
+    const userInfo = {
+      name: 'teste', 
+      email: 'teste@teste.com', 
+      picture: 'https://nerdhits.com.br/wp-content/uploads/2022/12/road-of-naruto-768x432.jpg', 
+      googleId: 11111111111111111111111
+    };
     const promise = login(userInfo);
     promise
       .then((r) => {
@@ -28,14 +33,17 @@ export default function Login() {
 
 	return (
 		<div className="container">
-      <GoogleOAuthProvider 
+      <button onClick={() => {
+          sucessGoogle();
+        } }> Logar!</button>
+      {/* <GoogleOAuthProvider 
       clientId="618107175710-ad2g5r7pivih84fp692gh1f70jkjnl9s.apps.googleusercontent.com">
         <GoogleLogin
 				buttonText="Continuar com o Google"
 				onSuccess={sucessGoogle}
 				onFailure={failGoogle}
 			  />    
-      </GoogleOAuthProvider>
+      </GoogleOAuthProvider> */}
 		</div>
 	);
 }
